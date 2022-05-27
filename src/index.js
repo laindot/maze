4c6f987d6e319027d5edbc4d3ecd9fc4e6725ed1
mecenas <jrafael.pfz@gmail.com>
2022-05-26 22:26:56 -0400

    [feature] - change maze height & width & add dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cellsHorizontal = 6;
-const cellsVertical = 4;
+const cellsHorizontal = 16;
+const cellsVertical = 11;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
